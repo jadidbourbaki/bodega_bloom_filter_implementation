018e01175525fb48c13f0be0cd3b79cce64f23f9
jadidbourbaki <jadidbourbaki@gmail.com>
2023-12-13 10:49:28 -0500

    Added tests

--- bodega_test.go
+++ bodega_test.go
@@ -5,14 +5,19 @@ import (
 	"testing"
 )
 
-func TestAccurateLearningModel(t *testing.T) {
+func PrepareRealSet(limit int) map[uint32]bool {
 	realSet := make(map[uint32]bool)
 
-	for i := 0; i < 100; i++ {
+	for i := 0; i < limit; i++ {
 		sample := rand.Uint32()
 		realSet[sample] = true
 	}
 
+	return realSet
+}
+
+func TestAccurateLearningModel(t *testing.T) {
+	realSet := PrepareRealSet(100)
 	lm := NewLearningModel(realSet, 1.0)
 
 	for value, _ := range realSet {
@@ -20,4 +25,33 @@ func TestAccurateLearningModel(t *testing.T) {
 			t.Fatalf(`All test values should return true: value %v`, value)
 		}
 	}
+}
+
+func TestInaccurateLearningModel(t *testing.T) {
+	limit := 1000
+
+	realSet := PrepareRealSet(limit)
+	secondSet := PrepareRealSet(limit)
+
+	lm := NewLearningModel(realSet, 0.0)
+
+	incorrect := 0
+
+	for value, _ := range realSet {
+		if !lm.Test(value) {
+			incorrect += 1
+		}
+	}
+
+	t.Log("False negatives:", incorrect, " Expected: ~", limit/2)
+
+	incorrect = 0
+
+	for value, _ := range secondSet {
+		if lm.Test(value) {
+			incorrect += 1
+		}
+	}
+
+	t.Log("False positives:", incorrect, " Expected: ~", limit/2)
 }
